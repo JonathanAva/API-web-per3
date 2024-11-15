33e4fe83725a8a43d1fb8367bc260e1c4a2db563
JonathanAva <hpalvarez10@gmail.com>
2024-11-15 04:35:46 -0600

    archivo para confirmar correo

--- controllers/userController.js
+++ controllers/userController.js
@@ -23,7 +23,7 @@ exports.verifyEmail = async (req, res) => {
   try {
     const { token } = req.query;
     const message = await userService.verifyEmail(token);
-    return res.sendFile('success.ejs', { root: './views' }); 
+    return res.sendFile('success.html', { root: './public' }); 
   } catch (error) {
     return res.status(400).send(error.message);
   }
